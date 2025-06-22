0ab58e5b90ac0705b1571abfa20d6c29a3e43721
vinicius <mvs_tj14@hotmail.com>
2025-06-22 00:36:29 -0300

    layout, localstorage, quantidade de acertos e cores ao clicar nos números

--- js/script.js
+++ js/script.js
@@ -5,9 +5,17 @@ $(document).ready(function () {
   const COLORS = {
     megasena: "#4CAF50",
     quina: "#2196F3",
-    lotofacil: "#FF9800",
-    lotomania: "#9C27B0",
+    lotofacil: "#9C27B0",
+    lotomania: "#FF9800",
   };
+
+  const HITS = {
+    megasena: 6,
+    quina: 5,
+    lotofacil: 15,
+    lotomania: 20,
+  };
+
   const SIZE_GAMES = {
     megasena: 60,
     quina: 80,
@@ -15,9 +23,18 @@ $(document).ready(function () {
     lotomania: 100,
   };
 
+  const numberHits = $('input[name="number-hits"');
+  numberHits.mask("00").on("keyup", function () {
+    if ($(this).val() > HITS[CONCURSO]) {
+      $(this).val(HITS[CONCURSO]);
+    }
+  });
+
   // HEADERS BUTTONS
-  const btnSortable = $("#btn-sortable");
   const btnMenuSortable = $(".btn-sortable");
+  btnMenuSortable.each(function () {
+    getLastResults($(this).data("game"), (data) => data);
+  });
 
   // API RESULTS
   let inputGame = $("input[name='name-game']");
@@ -29,11 +46,18 @@ $(document).ready(function () {
   let btnViewResult = $(".btn-view-result");
   inputGame.val(NAME_GAME);
 
-  let totalGames = $(".total-games");
+  // GAMES
+  const btnGenerateSortable = $("#btn-generate-numbers");
   let divContentNumbers = $(".div-content-numbers");
   let divTemplate = $(".div-template");
-  let divCard = $(".card");
   let divTitle = $(".card-title > h5");
+
+  btnMenuSortable.each(function () {
+    $(this).css({
+      color: COLORS[$(this).data("game")],
+      borderColor: COLORS[$(this).data("game")],
+    });
+  });
 
   btnMenuSortable.click(function () {
     btnMenuSortable.removeClass("active");
@@ -42,6 +66,7 @@ $(document).ready(function () {
     CONCURSO = $(this).data("game");
     divContentNumbers.hide("50");
     inputGame.val(NAME_GAME);
+    numberHits.val("");
   });
 
   btnViewResult.click(function () {
@@ -67,8 +92,8 @@ $(document).ready(function () {
       data.dezenas.forEach(function (number) {
         numberResult.append(`
           <div 
-            class="col rounded-circle" 
-            style="border: solid 1px ${COLORS[CONCURSO]}; width: 50px; height: 50px; display: flex; align-items: center; justify-content: center; font-size: 1.5rem;">
+            class="col rounded-circle text-light" 
+            style="border: solid 1px ${COLORS[CONCURSO]}; background-color: ${COLORS[CONCURSO]}; width: 32px; height: 32px; display: flex; align-items: center; justify-content: center;">
             ${number}
           </div>
         `);
@@ -78,6 +103,9 @@ $(document).ready(function () {
         <p>Valor acumulado do concurso: <strong>${formatMoney(
           data.valorAcumuladoProximoConcurso
         )}</strong></p>
+        <p>Valor acumulado para o próximo concurso: <strong>${formatMoney(
+          data.valorAcumuladoProximoConcurso
+        )}</strong></p>
         <p>Valor estimado para o próximo concurso: <strong>${formatMoney(
           data.valorEstimadoProximoConcurso
         )}</strong></p>
@@ -85,18 +113,61 @@ $(document).ready(function () {
     });
   });
 
-  btnSortable.click(function () {
-    divContentNumbers.show("50");
-    if (NAME_GAME != "Favoritos") {
-      console.log(COLORS[NAME_GAME], NAME_GAME);
+  btnGenerateSortable.click(function () {
+    let repeat = CONCURSO === "megasena" ? 6 : 5;
 
+    divTemplate.css({
+      display: "grid",
+      gridTemplateColumns: `repeat(${repeat}, 1fr)`,
+      gap: "10px",
+      alignItems: "center",
+      justifyItems: "center",
+    });
+
+    $(this).children().addClass("fa-spin");
+    sleep(800).then(() => {
+      divContentNumbers.show("50");
       divTitle.text(NAME_GAME);
-      divTitle.css("color", COLORS[NAME_GAME]);
-    }
+      divTitle.css("color", COLORS[CONCURSO]);
+
+      const numbersSoutable = randomNumbers(
+        SIZE_GAMES[CONCURSO],
+        HITS[CONCURSO],
+        CONCURSO,
+        numberHits.val()
+      );
+      divTemplate.empty();
+
+      numbersSoutable.forEach(function (number) {
+        divTemplate.append(`
+          <div
+            class="col rounded-circle btn-number-sortable"
+            style="border: solid 1px ${COLORS[CONCURSO]}; width: 32px; height: 32px; display: flex; align-items: center; justify-content: center;">
+            ${number}
+          </div>
+        `);
+      });
+
+      $(this).children().removeClass("fa-spin");
+      $(".btn-number-sortable").click(function () {
+        $(this).css({
+          color: "#ffffff",
+          backgroundColor: COLORS[CONCURSO],
+        });
+      });
+    });
   });
 });
 
 function getLastResults(concurso, callback) {
+  if (localStorage.getItem(`${concurso}`)) {
+    const cachedData = JSON.parse(localStorage.getItem(`${concurso}`));
+    if (cachedData.dataAtual == new Date().toLocaleDateString()) {
+      callback(cachedData);
+      return;
+    }
+  }
+
   $.ajax({
     url: `https://loteriascaixa-api.herokuapp.com/api/${concurso}/latest`,
     method: "GET",
@@ -106,8 +177,35 @@ function getLastResults(concurso, callback) {
     dataType: "json",
     success: function (response) {
       callback(response);
+      response.dataAtual = new Date().toLocaleDateString();
+      localStorage.setItem(`${concurso}`, JSON.stringify(response));
     },
   });
+}
+
+function randomNumbers(size_game, match_hits, consurso, number_hits) {
+  let numberResults = JSON.parse(localStorage.getItem(`${consurso}`));
+  let numbers = [];
+
+  if (number_hits) {
+    const previousNumbers = numberResults.dezenas.map(Number);
+    while (numbers.length < number_hits && previousNumbers.length > 0) {
+      let idx = Math.floor(Math.random() * previousNumbers.length);
+      let selected = previousNumbers[idx];
+      if (!numbers.includes(selected)) {
+        numbers.push(selected);
+      }
+    }
+  }
+
+  while (numbers.length < match_hits) {
+    let randomNumber = Math.floor(Math.random() * size_game) + 1;
+    if (!numbers.includes(randomNumber)) {
+      numbers.push(randomNumber);
+    }
+  }
+
+  return numbers.sort((a, b) => a - b);
 }
 
 function formatMoney(value) {
@@ -117,6 +215,6 @@ function formatMoney(value) {
   }).format(value);
 }
 
-function template(data) {}
-
-function templaterResuts(data) {}
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
